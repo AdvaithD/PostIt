74baa733279cb50d0162bc0533304bbcad9b6716
Advaith Doosa <advaithseo@gmail.com>
2019-07-06 15:12:39 -0700

    finished writing tests

--- test/DReddit_spec.js
+++ test/DReddit_spec.js
@@ -30,4 +30,29 @@ contract('DReddit', () => {
     assert.equal(web3.utils.hexToAscii(post.description), ipfsHash)
     assert.equal(post.owner, accounts[0])
   })
+
+  it('should not be able to vote in an unexisting post', async () => {
+    const userCanVote = await DReddit.methods.canVote('123').call()
+    assert.equal(userCanVote, false)
+  })
+
+  it('should be able to vote for in a post if account has not voted yet', async () => {
+    const userCanVote = await DReddit.methods.canVote(postId).call()
+    assert.equal(userCanVote, true)
+  })
+
+  it('should be able to vote in a post', async () => {
+    const receipt = await DReddit.methods.vote(postId, 1).send()
+    const Vote = receipt.events.NewVote
+    assert.equal(Vote.returnValues.owner, accounts[0])
+  })
+
+  it('should not be able to vote twice', async () => {
+    try {
+      const receipt = await DReddit.methods.vote(postId, 1).send()
+      assert.fail('should have reverted')
+    } catch (error) {
+      assert(error.message.search('revert') > -1, 'Revert should happen')
+    }
+  })
 })
