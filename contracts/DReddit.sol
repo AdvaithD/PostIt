1276e8dcb935648c71cab391d003cca15f337a6b
Advaith Doosa <advaithseo@gmail.com>
2019-07-06 12:27:33 -0700

    added upvote, downvote , NewVote event and initial votecount

--- contracts/DReddit.sol
+++ contracts/DReddit.sol
@@ -1,19 +1,30 @@
 pragma solidity ^0.5.0;
 
 contract DReddit {
+    enum Ballot { NONE, UPVOTE, DOWNVOTE }
 
     struct Post {
         uint creationDate;
         bytes description;
         address owner;
+        uint upvotes;
+        uint downvotes;
+        mapping(address => Ballot) voters;
     }
 
     Post[] public posts;
+    
   
    event NewPost(
        uint indexed postId,
        address owner,
        bytes decription
+   )
+
+   event NewVote(
+       uint indexed postId,
+       address owner,
+       uint8 vote
    )
 
     function createPost(bytes _description) public {
@@ -22,6 +33,8 @@ contract DReddit {
             creationDate: block.timestamp,
             description: _descriotion,
             owner: msg.sender
+            upvotes: 0,
+            downvotes: 0
         })
         emit NewPost(postId, msg.sender, _description)
     }
