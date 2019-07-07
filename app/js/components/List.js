053498b13960c86eb94d6082221aafef77f2e531
Advaith Doosa <advaithseo@gmail.com>
2019-07-06 22:06:40 -0700

    shifted logic over to app.js

--- app/js/components/List.js
+++ app/js/components/List.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
+import DReddit from '../../../embarkArtifacts/contracts/DReddit'
+// eslint-disable-next-line no-unused-vars
 import { Post } from './Post'
 
 export class List extends Component {
+    _isMounted = false;
   constructor (props) {
     super(props)
     this.state = {
       posts: []
     }
   }
+
   async componentDidMount () {
-    const totalPosts = await DReddit.methods.numPosts().call()
-
-    let list = []
-
-    for (let i = 0; i < totalPosts; i++) {
-      const post = DReddit.methods.posts(i).call()
-      list.push(post)
-    }
-    // Wait for all promises to get resolved
-    list = await Promise.all(list)
+        this._isMounted = true;
+    
   }
   render () {
+   const { posts } = this.state
     return (
-      <React.Fragemnt>
-        {this.state.posts.map(post => {
-          return (
-            <Post
+      <React.Fragment>
+      {this.props.posts.map(post => {
+       return (<Post
               key={post.id}
               description={post.description}
-              creationDate={post.creatioNDate}
+              creationDate={post.creationDate}
               owner={post.owner}
-            />
-          )
-        })}
-      </React.Fragemnt>
+            />)
+      })}
+      
+        
+      </React.Fragment>
     )
   }
 }
