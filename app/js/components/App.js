ff90fa508ebe57f8dfb915f4155791ef5f68cfde
Advaith Doosa <advaithseo@gmail.com>
2019-07-06 22:06:12 -0700

    moved most of the data handling to App.js

--- app/js/components/App.js
+++ app/js/components/App.js
@@ -2,15 +2,47 @@ import React, { Component } from 'react'
 import { CreatePost } from './CreatePost'
 import { Post } from './Post'
 import { List } from './List'
+import DReddit from '../../../embarkArtifacts/contracts/DReddit'
 
 export class App extends Component {
+   constructor (props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+   loadPosts = async () => {
+     const totalPosts = await DReddit.methods.numPosts().call();
+
+    let list = [];
+
+    if (totalPosts > 0) {
+      for (let i = 0; i < totalPosts; i++) {
+        const post = DReddit.methods.posts(i).call();
+        list.push(post);
+      }
+    }
+
+    list = await Promise.all(list);
+    list = list.map((post, index) => {
+      post.id = index;
+      return post;
+    });
+
+    list;
+
+    this.setState({ posts: list });
+   }
+   async componentDidMount() {
+     await this.loadPosts()
+   }
   render () {
     return (
       <React.Fragment>
-        <h1>DReddit</h1>
-        <CreatePost />
-        <List />
-
+        <h1>Decentralized Reddit</h1>
+        <CreatePost afterPostHandler={this.loadPosts.bind(this)}/>
+        <List posts={this.state.posts}/>
       </React.Fragment>
     )
   }
